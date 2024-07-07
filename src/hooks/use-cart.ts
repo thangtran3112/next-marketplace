e57daadca0cb42f2052981ed885c0fdf6168c4d7
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-07-07 14:44:43 -0700

    Product details page with Local storage for Add to cart button

---
+++ src/hooks/use-cart.ts
@@ -0,0 +1,38 @@
+import { Product } from "@/payload-types";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+export type CartItem = {
+  product: Product;
+};
+
+type CartState = {
+  items: CartItem[];
+  addItem: (product: Product) => void;
+  removeItem: (productId: string) => void;
+  clearCart: () => void;
+};
+
+/**
+ * Storing cart state in Local storage with persist and createJSONStorage from zustand
+ */
+export const useCart = create<CartState>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => {
+          return { items: [...state.items, { product }] };
+        }),
+      removeItem: (id) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.product.id !== id),
+        })),
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
